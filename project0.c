a43036136d022d365531a9983b7a93139fc211c1
Trey Lavender <trey.lavender@yahoo.com>
2020-02-28 23:07:01 -0600

    Update project0.c

--- project0.c
+++ project0.c
@@ -61,7 +61,7 @@ int main()
                                 dabbing[p].a = malloc(4 * sizeof(char));
                                 *dabbing[p].a = 'a';
                                 count = count + 1;
-                                printf("%s ", c);
+                                //printf("%s ", c);
                                 if(c < 0x0080)
                                 {
                                         *dabbing[p].a = c;
