1e55b8626d85128e4b97b912f42189086b49449f
Trey Lavender <trey.lavender@yahoo.com>
2020-02-28 20:49:59 -0600

    Update project0.c
    project0 ver 1.0

--- project0.c
+++ project0.c
@@ -1,7 +1,154 @@
+// Counts Unicode Characters and sorts them by highest to lowest.
+  
 #include <stdio.h>
-main() {
-  int num = 0;
-  scanf("%d", &num);
-  printf("%d\n", num);
-  return 0;
-}
+#include <stdlib.h> 
+#include <wchar.h>
+#include <locale.h>
+#define MAX_FILE_NAME 100 
+//Structure dab that holds a char array and interger for the number of times that Unicode character appear.
+struct dab
+{
+        unsigned char *a;
+        int b;
+};  
+int main() 
+{ 
+    //printf("Here");
+    //Intializing variables
+    FILE* fp = stdin; 
+    int count = 0;
+    int k = 0;
+    int m = -1;
+    int z = 0;
+    struct dab *dabbing = malloc(1114112 * sizeof(struct dab));
+    for(int i = 0; i < 1114112; i++)
+    {
+        dabbing[i].b = 0;
+        dabbing[i].a = malloc(4 * sizeof(char));
+        *dabbing[i].a = 'a';
+        //printf("%ls", dabbing[i].a);
+    }
+    unsigned char c;
+  //Recieving Unicode Characters from local
+    setlocale(LC_CTYPE, "");
+  //Input file
+    fp = stdin;  
+    int y = 0;
+    // Extract characters from file and store in c, compares with dabbing array values for matching or new characters. 
+    for (c = getc(fp); c != EOF; c = getc(fp)) 
+    {
+        k = 0;
+      //Duplicate Loop to check for repeating characters
+        for(int o = 0; o < 1114112; o++)
+        {
+                if(*dabbing[o].a == c && k == 0)
+                {
+                        //printf("Here2");
+                        dabbing[o].b = dabbing[o].b + 1;
+                        k = 1;
+
+                }
+        }
+        count = count + 1;
+      //Loop to place new character in proper position
+        if(k == 0)
+        {
+                //printf("Here1");
+                for(int p = 0; p < 1114112; p++)
+                {
+                        if(dabbing[p].b == 0 && k == 0)
+                        {
+                                //printf("%d ", d);
+                                if(c < 0x0080)
+                                {
+                                        *dabbing[p].a = c;
+                                        //printf("Byte:1");
+                                        dabbing[p].b = dabbing[p].b + 1;//printf("Byte:1");
+                                        dabbing[p].b = dabbing[p].b + 1;
+                                }
+                                else if(c < 0x00E0)
+                                {
+                                        *(dabbing[p].a) = c;
+                                        c = getc(fp);
+                                        *(dabbing[p].a + 1)  = c;
+                                        //printf("Byte:2");
+                                        dabbing[p].b = dabbing[p].b + 1;
+                                }
+                                else if(c < 0x00F0)
+                                {
+                                        *dabbing[p].a = c;
+                                        c = getc(fp);
+                                        *(dabbing[p].a + 1)  = c;
+                                        c = getc(fp);
+                                        *(dabbing[p].a + 2)  = c;
+                                        //printf("Byte:3");
+                                        dabbing[p].b = dabbing[p].b + 1;
+                                }
+                                else if(c < 0x00F8)
+                                {
+                                        *dabbing[p].a = c;
+                                        c = getc(fp);
+                                        *(dabbing[p].a + 1)  = c;
+                                        c = getc(fp);
+                                        *(dabbing[p].a + 2)  = c;
+                                        c = getc(fp);
+                                        *(dabbing[p].a + 3)  = c;
+                                        //printf("Byte:4");
+                                        dabbing[p].b = dabbing[p].b + 1;
+                                }
+                                else
+                                {
+                                        //printf("Breakdown");
+                                        y = 1;
+                                }
+                                //*dabbing[p].a =  c;
+                                //printf("%s", dabbing[p].a);
+                                //dabbing[p].b = dabbing[p].b + 1;
+                                k = 1;
+                        }
+                } 
+        }
+        //counting[characterNum] = counting[characterNum] + 1;
+        if(y == 1)
+        {
+                break;
+        }
+}  
+    // While loop to sort arrays from highest to lowest
+    k = 0;
+    //printf("%s->%d\n", dabbing[0].a, dabbing[0].b);
+    while(k == 0)
+    { 
+        k = 1;
+        for(int l = 0; l < 1114112; l++)
+        {
+                if(dabbing[l].b != 0)
+
+                {
+                        k = 0;
+                }
+        }
+         for(int j = 0; j < 1114112; j++)
+         {
+                if(dabbing[j].b > m)
+                {
+                        m = dabbing[j].b;
+                        //printf("%c->%d\n", n, m);
+                        z = j;
+                        //printf("%d", m);
+                }
+                if(j == 1114111)
+                {
+                        dabbing[z].b = 0;
+                }
+         }
+        if(k == 0)
+        {
+                printf("%s->%d\n", dabbing[z].a, m);
+        }
+        m = -1;
+    }  
+    // Close the file 
+    fclose(fp);
+    return 0; 
+} 
